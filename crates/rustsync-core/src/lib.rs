e78e998e348af04bc823df99a0425ce9caf8f5cc
evgen2571 <evgen.10.02.2008.83@gmail.com>
2026-05-30 14:28:44 +0300

    feat(scanner): implement simple dir scanner (scan function)

--- crates/rustsync-core/src/lib.rs
+++ crates/rustsync-core/src/lib.rs
@@ -1,3 +1,1 @@
-pub fn print_hello() {
-    println!("Hello, World");
-}
+pub mod scanner;
---
+++ crates/rustsync-core/src/scanner.rs
@@ -0,0 +1,52 @@
+use std::{
+    fs, io,
+    path::{Path, PathBuf},
+};
+use walkdir::WalkDir;
+
+#[derive(Debug, Clone)]
+pub enum EntryKind {
+    File,
+    Dir,
+}
+
+#[derive(Debug, Clone)]
+pub struct ScanEntry {
+    pub relative_path: PathBuf,
+    pub kind: EntryKind,
+}
+
+pub fn scan_dir(root: impl AsRef<Path>) -> io::Result<Vec<ScanEntry>> {
+    let root = root.as_ref().canonicalize()?;
+    let mut entries = Vec::new();
+
+    for item in WalkDir::new(&root) {
+        let item = item?;
+
+        let path = item.path();
+        if path == root {
+            continue;
+        }
+
+        let relative_path = path.strip_prefix(&root).unwrap().to_path_buf();
+        let metadata = fs::metadata(path)?;
+
+        if metadata.is_dir() {
+            entries.push(ScanEntry {
+                relative_path,
+                kind: EntryKind::Dir,
+            });
+
+            continue;
+        }
+
+        if metadata.is_file() {
+            entries.push(ScanEntry {
+                relative_path,
+                kind: EntryKind::File,
+            });
+        }
+    }
+
+    Ok(entries)
+}
